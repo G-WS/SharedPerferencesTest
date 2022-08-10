aa741e1e862e64eab3abe7583be5d3f1f9c3c9c0
G-WS <3355139577@qq.com>
2022-08-10 22:02:42 +0800

    0810

--- app/src/main/java/com/example/sharedperferencestest/MainActivity.java
+++ app/src/main/java/com/example/sharedperferencestest/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.sharedperferencestest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedReader;
 
 public class MainActivity extends AppCompatActivity {
-
+private String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.save_data);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("name","Tom");
+                editor.putInt("age",22);
+                editor.putBoolean("married",false);
+                editor.apply();
+            }
+        });
+        Button button1 = (Button) findViewById(R.id.restore_data);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences pref = getSharedPreferences("data",MODE_PRIVATE);
+                String name = pref.getString("name","");
+                int age = pref.getInt("age",0);
+                boolean married = pref.getBoolean("married",false);
+                Log.d(TAG, "name is "+name);
+                Log.d(TAG, "age is "+age);
+                Log.d(TAG, "married is "+married);
+            }
+        });
     }
 }
